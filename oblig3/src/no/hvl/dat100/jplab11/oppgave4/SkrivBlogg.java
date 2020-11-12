70ac6ca95f1e9ec2cc19a755a5e32eba37c0156c
a595834 <69847974+a595834@users.noreply.github.com>
2020-11-12 23:04:38 +0100

    Add files via upload

---
+++ oblig3/src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -0,0 +1,25 @@
+package no.hvl.dat100.jplab11.oppgave4;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+import no.hvl.dat100.jplab11.common.TODO;
+import no.hvl.dat100.jplab11.oppgave3.*;
+
+public class SkrivBlogg {
+
+	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+
+		try {
+			PrintWriter fil = new PrintWriter(mappe + filnavn);
+			
+			fil.write(samling.toString());
+			fil.close();
+			return true;
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+}
